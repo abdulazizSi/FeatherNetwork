81c11c903547b542d3baf4f7948ffb1506b937f4
Abdulaziz ALSIKH <abdulaziz@elephantapps.co>
2023-09-26 10:28:25 +0300

    created server paths enum

---
+++ FeatherNetwork/NetworkService/Compontnents/ServerPaths.swift
@@ -0,0 +1,13 @@
+//
+//  ServerPaths.swift
+//  FeatherNetwork
+//
+//  Created by Abdulaziz Alsikh on 26.09.2023.
+//
+
+import Foundation
+
+enum ServerPath: String {
+    case example = "example/example"
+    
+}
